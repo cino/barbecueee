1a395576b0284d50e58495f9789e89c1f8f1db63
Ricardo Cino <ricardo@cino.io>
2017-11-18 22:10:29 +0100

    It workz!

--- src/extension.ts
+++ src/extension.ts
@@ -1,15 +1,17 @@
 'use strict';
 import * as vscode from 'vscode';
-import { ShellExecution } from 'vscode';
+import { exec } from 'child_process';
+import { workspace } from 'vscode';
 
 export function activate(context: vscode.ExtensionContext) {
 
     let disposable = vscode.commands.registerCommand('extension.barbecueee', () => {
 
-        // ShellExecution.apply('git reset --hard && git clean -df');
-        // ShellExecution.apply('echo "test"');
+        let folders = workspace.workspaceFolders;
+        let projectFolder = folders[0]['uri']['path'] 
 
-
+        exec('cd ' + projectFolder + ' && git reset --hard && git clean -df');
+        
         vscode.window.showInformationMessage('who wantz to be a burgerrr 🍔🔥');
     });
 
